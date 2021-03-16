7c59fe5bb293729fdff6a3d7e03b346b87de8860
David McFarland <corngood@gmail.com>
2021-03-16 13:38:19 -0300

    use stderr for log

--- UnityDebug/Log.cs
+++ UnityDebug/Log.cs
@@ -8,15 +8,7 @@ namespace UnityDebug
 {
 	public static class Log
 	{
-		static readonly string logPath;
-
 		public static bool Debug { get; set; }
-
-		static Log ()
-		{
-			logPath = Path.Combine(Path.GetDirectoryName (Assembly.GetExecutingAssembly ().Location), Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly ().Location) + "-log.txt");
-			File.WriteAllText (logPath, FormatMessage("Log\n---\n"));
-		}
 
 		static string FormatMessage(string message)
 		{
@@ -38,11 +30,7 @@ namespace UnityDebug
 		public static void Write(string message)
 		{
 			var formattedMessage = FormatMessage (message);
-			var buf = Encoding.UTF8.GetBytes(formattedMessage);
-			using (var stream = new FileStream(logPath, FileMode.Append, FileAccess.Write, FileShare.ReadWrite))
-			{
-				stream.Write(buf, 0, buf.Length);
-			}
+			Console.Error.Write(formattedMessage);
 		}
 	}
 }
